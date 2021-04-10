ecce7bb2a22bc6e60c66dd44b83a42e14abf99c2
ag1363 <ag1363@gmail.com>
2021-04-10 13:20:21 +0430

    b

--- pages/product.vue
+++ pages/product.vue
@@ -36,29 +36,59 @@
                 			<div class="fs-18 gray--text">توضیحات</div>
                 			<div class="w-50 fs-18 gray--text">شیر پر چرب هراز حجم 1 لیتر شیر پر چرب هراز حجم 1 لیتر شیر پر چرب هراز حجم 1 لیتر شیر پر چرب هراز حجم 1 لیتر شیر پر چرب هراز حجم 1 لیتر </div>
                 		</div>
-                		<div class="prod-acart w-50">
-                			<div class="">
+                		<div class="prod-acart">
+                			<div class="d-ib vam">
                 				<div class="gray--text fw-400 td-lt">120,000 ریال</div>
                 				<div class="fs-22 fw-500 mt-4 success--text">110,000 <span class="gray--text"> ریال</span></div>
                 			</div>
-                			<div class=""></div>
-                			<div class=""></div>
+                			<div class="d-ib mr-10">
+                				<div class="d-ib vam pmsec">
+                					<div><img class="plus" src="images/home/plus.png"></div>
+                					<div><img class="minus" src="images/home/minus.png"></div>
+                				</div>
+                				<div class="d-ib"><input class="ccount" type="number" value="1"></div>
+                			</div>
+                			<div class="d-ib mr-10">
+                                <v-btn @click="addCart" color="success" elevation="1" block x-large>
+                                    افزودن به سبد خرید
+                                </v-btn>
+                			</div>
                 		</div>
                 	</div>
                 </div>
             </div>
         </div>
 
+        <div class="row mb-10">
+            <div class="products-box mt-8">
+                <div class="darkGray--text mb-2 mx-2">محصولات مشابه</div>
+                <div class="swiper-wrapper">
+                    <SwiperSlideProductDesktop v-for="i in 10"
+                        :key="i"
+                        :id="i"
+                        name="گوشت چرخ کرده مهیا"
+                        priceDiscount="320000"
+                        price="300000"
+                        image="/images/home/product.png" />
+                </div>
+            </div>
+        </div>
+
+
     </div>
 </template>
 
 <script>
-// import Swiper from 'swiper';
-// import 'swiper/css/swiper.css';
+import Swiper from 'swiper';
+import 'swiper/css/swiper.css';
 export default {
     layout: 'home',
     mounted(){
         this.$store.commit('_HOME', ['wmenu', 0]);
+        new Swiper('.products-box', {
+            slidesPerView: 'auto',
+            spaceBetween: 25,
+        });
     },
     data(){
         return{
@@ -70,14 +100,15 @@ export default {
         }
     },
     methods:{
-        register(){
-            alert('registered!')
+        addCart(){
+            alert('added to cart!')
         }
     },
 };
 </script>
 
 <style>
+.d-ib{display: inline-block;}
 .td-lt{text-decoration: line-through;}
 .w-50{width: 50%;}
 .vam{vertical-align: middle;}
@@ -120,6 +151,28 @@ export default {
 }
 .prod-acart{
 	padding-top: 25px;
+}
+.prod-acart img{
+	width: 26px;
+	height: 26px;
+	border: 1px solid;
+	border-radius: 3px;
+}
+.prod-acart .pmsec{
+	padding: 10px 4px 0;
+}
+.prod-acart .plus{
+	color: #16c959;
+}
+.prod-acart .minus{
+	color: #f33542;
+}
+.prod-acart .ccount{
+	width: 62px;
+	height: 62px;
+	border: 2px solid #ccc;
+    border-radius: 5px;
+    text-align: center;
 }
 
 .slider-default{
