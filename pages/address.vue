d39c14e1641c70dda47808a9333e8b814a076919
ag1363 <ag1363@gmail.com>
2021-04-14 10:41:40 +0430

    g

--- pages/address.vue
+++ pages/address.vue
@@ -1,35 +1,95 @@
 <template>
     <div class="address-wrapper">
 
-        <div class="address" v-for="i in 2">
-            <div class="map">
-                <img src="/images/tmp/map.jpg">
+
+        <div class="fs-14 fw-600 mt-6 mb-2">حساب کاربری من</div>
+        <div class="row">
+            <div class="col-4">
+                <div class="account-menu fs-14">
+                    <nuxt-link class="sl" to="/profile">
+                        <div>
+                            <img src="/images/account/user.svg">
+                            پروفایل من
+                        </div>
+                    </nuxt-link>
+                    <nuxt-link class="sl" to="/wallet">
+                        <div>
+                            <img src="/images/account/wallet.svg">
+                            کیف پول من
+                        </div>
+                    </nuxt-link>
+                    <nuxt-link class="sl active" to="/address">
+                        <div>
+                            <img src="/images/account/signs.svg">
+                            آدرس های من
+                        </div>
+                    </nuxt-link>
+                    <nuxt-link class="sl" to="/share">
+                        <div>
+                            <img src="/images/account/share.svg">
+                            ثبت و ارسال کد معرف
+                        </div>
+                    </nuxt-link>
+                    <nuxt-link class="sl" to="/gift">
+                        <div>
+                            <img src="/images/account/gift.svg">
+                            کدهای هدیه من
+                        </div>
+                        <div class="l">
+                            <v-badge
+                                color="primary"
+                                content="2">
+                            </v-badge>
+                        </div>
+                    </nuxt-link>
+                    <nuxt-link class="sl" to="/wallet">
+                        <div>
+                            <img src="/images/account/wallet.svg">
+                            لیست خرید من
+                        </div>
+                    </nuxt-link>
+                    <nuxt-link class="sl" to="/support">
+                        <div>
+                            <img src="/images/account/support.svg">
+                            ارتباط با پشتیبانی
+                        </div>
+                    </nuxt-link>
+                </div>
             </div>
-            <div class="mt-2">
-                <div class="sl">
-                    <div class="primary--text">خانه</div>
-                    <div class="l">
-                        <img src="/images/address/edit_bl.png">
-                        <img src="/images/address/error.svg">
+            <div class="col-6">
+                <div class="row">
+                    <div class="col-6 address" v-for="i in 2">
+                        <div class="map">
+                            <img src="/images/tmp/map.jpg">
+                        </div>
+                        <div class="mt-2">
+                            <div class="sl">
+                                <div class="primary--text">خانه</div>
+                                <div class="l">
+                                    <img src="/images/address/edit_bl.png">
+                                    <img src="/images/address/error.svg">
+                                </div>
+                            </div>
+                            <div class="sl">
+                                <div class="gray--text fs-12">یزد، امامشهر، انتهای بلوار کارگر، خ شهید حسین کیا، کوچه توحید</div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div class="sl">
-                    <div class="gray--text fs-12">یزد، امامشهر، انتهای بلوار کارگر، خ شهید حسین کیا، کوچه توحید</div>
+                <div class="row">
+                    <div class="col">
+                        <div class="checkout  mt-10">
+                            <v-btn
+                                to="newaddress"
+                                color="primary"
+                                rounded
+                                x-large>
+                                افزودن آدرس جدید
+                            </v-btn>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-
-        <div class="add-address">
-            <v-btn
-                to="/selectAddress"
-                class="mx-2"
-                fab
-                dark
-                color="accent">
-                <v-icon dark>
-                    mdi-plus
-                </v-icon>
-            </v-btn>
         </div>
 
     </div>
@@ -37,7 +97,7 @@
 
 <script>
 export default {
-    layout: 'default-with-header',
+    layout: 'home',
     mounted(){
         this.$store.commit('_HOME', ['title', 'آدرس های من']);
     },
@@ -51,6 +111,53 @@ export default {
 </script>
 
 <style scoped>
+.w-50{width: 50%;}
+.db{display: block !important;}
+.account-menu{
+    border: 1px solid #eee;
+    border-radius: 10px;
+    border-radius: 15px;
+    overflow: hidden;
+}
+.account-menu .sl{
+    width: 100%;
+    display: inline-block;
+    padding: 20px 15px;
+    margin-bottom: 0;
+    border-bottom: 1px solid #eee;
+    color: unset;
+}
+.account-menu .sl:last-child{
+    border-bottom: 0px;
+}
+.account-menu .sl div{
+    display: inline-block;
+}
+.account-menu .sl .l{
+    float: left;
+    margin: 5px 14px -5px;
+}
+.account-menu .sl.btns{
+    text-align: center;
+    margin-top: 20px;
+}
+.account-menu .sl.btns > div{
+    padding: 0 5px;
+    width: 48%;
+}
+.account-menu .sl.btns button{
+    height: 40px;
+}
+.account-menu img{
+    width: 18px;
+    vertical-align: middle;
+    margin-left: 10px;
+}
+.account-menu .active{
+    background-color: #5e5d5e;
+    color: #fff;
+}
+
 .address-wrapper{
     padding-bottom: 60px;
 }
---
+++ pages/newaddress.vue
@@ -0,0 +1,145 @@
+<template>
+    <div class="">
+
+
+        <div class="fs-14 fw-600 mt-6 mb-2">حساب کاربری من / آدرس های من / افزودن آدرس جدید</div>
+
+        <div class="row">
+            <div class="col products-box mt-8">
+
+                <div class="row mt-8">
+                    <div class="col-3"></div>
+                    <div class="col-6">
+                        <div class="row">
+                            <div class="col a-select-wrapper">
+                              <label>استان</label>
+                              <select>
+                                <option>-</option>
+                                <option>تهران</option>
+                                <option>یزد</option>
+                              </select>
+                              <i class="material-icons">keyboard_arrow_down</i>
+                            </div>
+                            <div class="col a-select-wrapper">
+                              <label>شهرستان</label>
+                              <select>
+                                <option>-</option>
+                              </select>
+                              <i class="material-icons">keyboard_arrow_down</i>
+                            </div>
+                        </div>
+                        <div class="row mt-4">
+                            <div class="col-6"><a-input label="عنوان آدرس" required/></div>
+                        </div>
+                        <div class="row">
+                            <div class="col"><a-input label="آدرس کامل" required/></div>
+                        </div>
+                        <div class="row">
+                            <div class="col-12 fs-11">انتخاب آدرس روی نقشه</div>
+                            <div class="col-12">
+                                <img src="images/tmp/map.jpg">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-8"></div>
+                            <div class="col-4">
+                                <v-btn @click="register" color="success" elevation="1" block x-large>
+                                    تایید و ثبت آدرس
+                                </v-btn>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-3"></div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+// import Swiper from 'swiper';
+// import 'swiper/css/swiper.css';
+export default {
+    layout: 'home',
+    mounted(){
+        this.$store.commit('_HOME', ['wmenu', 0]);
+    },
+    data(){
+        return{
+            /*
+            address:{
+                name: 'خونه',
+                address: 'یزد امام شهر، انتهای بلوار کارگر',
+            },
+            */
+        }
+    },
+    methods:{
+        register(){
+            alert('registered!')
+        }
+    },
+    /*
+    computed:{
+        selectedShop(){
+            return this.$store.state._home.selectedShop;
+        }
+    }*/
+};
+</script>
+
+<style>
+.vam{
+    vertical-align: middle;
+}
+.categories{
+    background-color: #fff;
+    box-shadow: 0 2px 15px -5px rgb(0,0,0,.20);
+    border-radius: 5px;
+}
+.categories .categories-header{
+    background-color: #f2f2f2;
+}
+.categories ul{
+    list-style: none;
+    margin: 0;
+    padding: 0;
+}
+.categories li{
+    border-bottom: 1px solid #eee;
+}
+.categories a{
+    padding: 6px;
+    display: block;
+    text-decoration: none;
+}
+.slider-default{
+    overflow: hidden;
+}
+.app-area{
+    /*background-color: rgb(248,248,247);*/
+}
+.app-area-dls{
+    position: absolute;
+    margin-top: 230px;
+    right: calc(50% - 500px);
+}
+.app-area-dl{
+    display: block;
+    margin-top: 14px;
+    border-radius: 6px;
+}
+
+
+
+.deactive{
+    opacity: 0;
+}
+.addresses{
+    border: 1px solid lightgray;
+    border-radius: 10px;
+    padding: 15px 10px;
+    margin-bottom: 10px;
+}
+</style>
